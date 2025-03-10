88098b0c6ea4f187c3f3c2068901442ee1520724
ArivAg18 <ariv.agarwal@gmail.com>
2025-03-11 00:59:35 +0530

    KM to Miles

---
+++ KmMiles.java
@@ -0,0 +1,19 @@
+import java.util.Scanner; // Import Scanner class
+
+class KmMiles {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        double km;
+
+        System.out.print("Enter distance in kilometers: ");
+        km = input.nextDouble(); 
+        
+        double miles = km / 1.6;
+
+        System.out.println(km + " kilometers is equal to " + miles + " miles.");
+
+        input.close();
+    }
+}
