0143e9dbbfb36f29e3df15a27632277e47d64d06
ArivAg18 <ariv.agarwal@gmail.com>
2025-03-11 00:49:33 +0530

    converter

---
+++ converter.java
@@ -0,0 +1,10 @@
+ class converter {
+    public static void main(String[] args) {
+        // Given distance in kilometers
+        double kilometers = 10.8;
+
+        double miles = kilometers / 1.6;
+
+        System.out.println(kilometers + " kilometers is equal to " + miles + " miles.");
+    }
+}
